711e657ede397d655c0b59d8cc82d55f496f4f0a
CondoriM <condoria.miguel@gmail.com>
2022-05-09 16:00:34 +0200

    axios api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeadSite/>
+    <form @submit.prevent="search">
+      <input type="text" v-model="searchT">
+      <button type="submit">Invia</button>
+      <div v-for="(film,index) in films" :key="index">
+        {{film.original_title}}
+      </div>
+    </form>
+    
+    {{searchT}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import HeadSite from '@/components/HeadComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HeadSite
+  },
+
+  data(){
+    return{
+      searchT: '',
+      films: '',
+      apy_key: '626f23522e216489e48887e2688ac97f',
+    }
+  },
+
+  methods:{
+    search(){
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.apy_key}&language=it-IT&page=1&include_adult=false&query=${this.searchT}`).then(response =>{
+      this.films = response.data.results
+      console.log(this);
+      console.log(this.films);
+    })
+    }
+  },
 }
 </script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/HeadComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container-fluid">
+        <div class="navbar bg-dark">
+            <div class="col-2">
+
+            </div>
+
+            <div class="col">
+                <input
+                    type="text"
+                    :value="search"
+                    @input="$emit('update:search', $event.target.value)"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'HeadSite',
+    props: ['search'],
+    emits: ['update:search']
+}
+</script>
